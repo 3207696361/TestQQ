7b158a9b8a23c86a6347dc98fbc91d71abf1b154
3207696361 <3207696361@qq.com>
2017-03-22 11:48:07 +0800

    --dfghj

---
+++ app/src/main/java/com/example/textqq/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.textqq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.textqq.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
